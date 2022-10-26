f8107077e51afe2691cdf31ecc9d4527da1ddfd0
dzp <2360591410@qq.com>
2022-10-27 02:35:01 +0800

    feat:目录修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,79 +25,81 @@ module.exports = {
         ],
         sidebar: [
             {
-                title: "欢迎学习",
-                path: '/',
-                collapsable: false,  // 是否折叠
-                children: [{ title: "博客简介", path: "/" }],
-            },
-            {
-                title: "基础篇",
-                collapsable: true,
+                title: "HTML",
+                
                 children: [
                     {
-                        title: "HTML",
-                        collapsable: true,
-                        children: [
-                            {
-                                title: "HTML媒体标签之video",
-                                path: './basicknowledge/HTML/mediaLabel-video.md'
-                            },
-                            {
-                                title: "HTML媒体标签之audio",
-                                path: './basicknowledge/HTML/mediaLabel-audio.md'
-                            },
-                            {
-                                title: "HTML媒体标签之track",
-                                path: './basicknowledge/HTML/mediaLabel-track.md'
-                            },
-                            {
-                                title: "HTML媒体标签之source",
-                                path: './basicknowledge/HTML/mediaLabel-source.md'
-                            },
-                            {
-                                title: "HTML媒体标签之embed",
-                                path: './basicknowledge/HTML/mediaLabel-embed.md'
-                            },
-                            {
-                                title: "HTML媒体标签之object param",
-                                path: './basicknowledge/HTML/mediaLabel-object-param.md'
-                            },
-                        ]
+                        title: "HTML媒体标签之video",
+                        path: './basicknowledge/HTML/mediaLabel-video.md'
                     },
                     {
-                        title: "css",
-                        collapsable: true,
-                        children: [
-                            {
-                                title: 'flex布局',
-                                path: './basicknowledge/CSS/flex.md'
-                            },
-                            {
-                                title: 'grid布局',
-                                path: './basicknowledge/CSS/grid.md'
-                            },
-                        ]
+                        title: "HTML媒体标签之audio",
+                        path: './basicknowledge/HTML/mediaLabel-audio.md'
                     },
                     {
-                        title: "JavaScript",
-                        collapsable: true,
-                        children: [
-                            {
-                                title: '数据类型',
-                                path: './basicknowledge/JavaScript/dataType.md'
-                            },
-                            {
-                                title: 'instanceof',
-                                path: './basicknowledge/JavaScript/instanceof.md'
-                            },
-                            {
-                                title: '常用技巧',
-                                path: './basicknowledge/JavaScript/commonSkills.md'
-                            }
-                        ]
+                        title: "HTML媒体标签之track",
+                        path: './basicknowledge/HTML/mediaLabel-track.md'
+                    },
+                    {
+                        title: "HTML媒体标签之source",
+                        path: './basicknowledge/HTML/mediaLabel-source.md'
+                    },
+                    {
+                        title: "HTML媒体标签之embed",
+                        path: './basicknowledge/HTML/mediaLabel-embed.md'
+                    },
+                    {
+                        title: "HTML媒体标签之object param",
+                        path: './basicknowledge/HTML/mediaLabel-object-param.md'
                     },
                 ]
-            }
+            },
+            {
+                title: "css",
+                
+                children: [
+                    {
+                        title: 'flex布局',
+                        path: './basicknowledge/CSS/flex.md'
+                    },
+                    {
+                        title: 'grid布局',
+                        path: './basicknowledge/CSS/grid.md'
+                    },
+                ]
+            },
+            {
+                title: "JavaScript",
+                
+                children: [
+                    {
+                        title: '数据类型',
+                        path: './basicknowledge/JavaScript/dataType.md'
+                    },
+                    {
+                        title: 'instanceof',
+                        path: './basicknowledge/JavaScript/instanceof.md'
+                    },
+                    {
+                        title: '常用技巧',
+                        path: './basicknowledge/JavaScript/commonSkills.md'
+                    }
+                ]
+            },
+            {
+                title: "Vue",
+                children: [
+                    {
+                        title: 'Vue3',
+                        path: './basicknowledge/Vue/Vue3.0.md'
+                    },
+                    {
+                        title: 'Vue2',
+                        path: './basicknowledge/Vue/Vue2.0.md'
+                    },
+                ]
+            },
+        
         ]
     }
 }
